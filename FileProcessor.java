fad42c60848a053a52473e20644a1124e75abe78
Erinthia <erinthiadinda167@gmail.com>
2025-03-14 21:55:56 +0700

    menambahkan deklarasi komponen UI

---
+++ FileProcessor.java
@@ -0,0 +1,12 @@
+package PemrogramanJaringan;
+
+import javax.swing.*;
+
+public class FileProcessor {
+    // Deklarasi komponen UI
+    private JTextArea textArea; // Area teks untuk menampilkan isi file
+    private JLabel lineCountLabel, wordCountLabel, charCountLabel; // Label untuk menampilkan jumlah baris, kata, dan
+                                                                   // karakter
+    private JTextField filePathField; // Field teks untuk menampilkan path file yang dipilih
+
+}
